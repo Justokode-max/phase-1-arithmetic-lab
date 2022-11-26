aeab715b989d7e559a6241d90ebc18a468ade4b6
Justus Dianga Okode <justusdiangaokode@gmail.com>
2022-11-26 09:16:33 +0300

    Added four variables that did various arithmetic operations

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1=31;
+let num2=2;
+let num3=16;
+let num4=12;
+const multiply=(num1*num2)
+const random = Math.floor(Math.random()*10)+1;  
+const mod=num3%num4;    
 
-// Write your code here
+   
+   
+
+
+const max= Math.max(0,20)
